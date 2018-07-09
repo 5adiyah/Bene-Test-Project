fd5b36c06c04eefc7845f39f2aa5aea1651ed0bb
5adiyah <sadiali@pdx.edu>
2018-07-09 14:40:13 -0700

    Fix style.scss missing error and update folder structure

---
+++ web/themes/sadio_frgk/js/main-dist.js
@@ -0,0 +1,1 @@
+console.error("Error: Cannot find module '/Users/ts-intern2/Sites/bene-test-project/web/themes/sadio_frgk/js/main.js' from '/Users/ts-intern2/Sites/bene-test-project/web/themes/sadio_frgk'");
